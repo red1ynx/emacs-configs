5e515a18283a1e4d72f24e2c84753f5639a5d054
Dzmitry Hancharou <red1ynx@tut.by>
2011-12-26 10:24:28 +0300

    Changes for work machine.

--- config/custom.el
+++ config/custom.el
@@ -1,3 +1,7 @@
+;; get rid of startup screen
+(setq inhibit-splash-screen t)
+(setq initial-scratch-message "")
+
 ;; color-theme
 (add-to-list 'load-path (concat plugins-directory "color-theme-6.6.0"))
 (require 'color-theme)
@@ -8,8 +12,10 @@
 (global-font-lock-mode t)
 
 ;; font
-(when im-at-home
+(when (eq system-type 'gnu/linux)
   (set-default-font "Monospace-12"))
+(when (eq system-type 'windows-nt)
+  (set-default-font "Courier New-12"))
 
 ;; title format
-(setq frame-title-format '("" invocation-name " - " "%b"))
+(setq frame-title-format '("" user-login-name "" invocation-name " - " "%b"))
--- init.el
+++ init.el
@@ -1,8 +1,11 @@
 ;; Settings for home machine
-(when (string-equal "red1ynx" (system-name))
+(when (string-equal "dimas" (user-login-name))
   (setq dropbox-directory "~/notes/Dropbox/")
   (setq im-at-home t))
-;; TODO Settings for working machine
+;; Setting for work machine
+(when (string-equal "Dzmitry_Hancharou" (user-login-name))
+  (setq dropbox-directory "d:/Home/dropbox/")
+  (setq im-at-work t))
 
 (setq plugins-directory (concat dropbox-directory "emacs/plugins/"))
 
