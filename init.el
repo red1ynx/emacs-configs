434855e12e82d9b1a83d708956e861fe257d3e60
Dzmitry Hancharou <red1ynx@tut.by>
2013-12-23 23:50:02 +0300

    Added elpa repositories.

--- init.el
+++ init.el
@@ -17,8 +17,9 @@
 
 ;;; Packaging
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
 ;;; Customization
